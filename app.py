72433521e759d0204f9bc570430b7c354be9e6b6
Ivander <ivandersalvadorruiz@gmail.com>
2024-03-20 13:30:31 -0300

    feat: login

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:1234@localhost:3306/flask_auth"
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso"}), 200
+
+
+    return jsonify({"message": "Credenciais Inválidas"}), 400
+
 
 @app.route("/hello", methods=["GET"])
 def hello():
